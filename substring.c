d9413b0c35b850265baf103b845bcdee72b31349
Thomas <thomulusparker@gmail.com>
2016-11-25 15:40:13 -0800

    substring.c

---
+++ substring.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int substring(char *source, int from, int n, char *target){
+	if ((unsigned)strlen(source)  -1 < from + n){
+		return -1;
+	}
+	if (((unsigned)strlen(source)  -1 < from) | ((unsigned)strlen(source) < n)){
+		return -1;
+	}
+	char *iterator = source;
+	for (int i = 0; i < from; i ++) {
+		iterator ++;
+		//printf("%c",*iterator);
+	}
+	char result[n];
+	for (int i = 0; i < n; i ++) {
+		result[i] = *iterator;
+		iterator++;
+	}
+	char *ptr = NULL;
+	for (int i = 0; i <n; i++){
+		ptr = &result[i];
+		*target = *ptr;
+		target++;
+
+	}
+	return 0;
+}
+int main(){
+	char *hello = "itstheendoftheworldasweknowit";
+	int index = 11;
+	char target[100] = "";
+	substring(hello, index, 10, target);
+	printf("%s",target);
+	return 0;
+}
